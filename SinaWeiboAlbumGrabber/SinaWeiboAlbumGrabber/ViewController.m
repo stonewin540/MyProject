9eb2c4737e90c7985a2bd386e34145006ad30c85
stonewin540 <stonewin540@163.com>
2015-01-28 21:19:54 +0800

    A new project for fetch image from sina weibo

---
+++ SinaWeiboAlbumGrabber/SinaWeiboAlbumGrabber/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SinaWeiboAlbumGrabber
+//
+//  Created by stone win on 1/28/15.
+//  Copyright (c) 2015 stone win. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
